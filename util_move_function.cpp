944b8fe2a0da384e6a606cb27f9f8f5a78fc0513
Vivek Kumar <118689020+vickvey@users.noreply.github.com>
2023-09-22 09:38:57 +0000

    std::move demo is added as stl_move_function.cpp

---
+++ util_move_function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <utility>
+
+int main() {
+    // Create two objects, 'source' and 'destination'
+    std::string source = "I am the source.";
+    std::string destination = "I am the destination.";
+
+    // Use std::move to transfer ownership from 'source' to 'destination'
+    destination = std::move(source);
+
+    // Now, 'destination' has the original content of 'source'
+    std::cout << "After move, destination: " << destination << std::endl;
+
+    // 'source' is left in a valid but unspecified state
+    std::cout << "After move, source: " << source << std::endl;
+
+    return 0;
+}
